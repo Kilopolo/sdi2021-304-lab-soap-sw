1e03b665c4e34319403baf15ab0a0b472186e439
UO271245 <71383208+UO271245@users.noreply.github.com>
2021-04-28 20:53:33 +0200

    First commit

---
+++ sdi2021-101-lab-soap-sw/src/main/java/com/example/demo/Sdi2021101LabSoapSwApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2021101LabSoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2021101LabSoapSwApplication.class, args);
+	}
+
+}
